fd817114f6193058dffe244afaccfcc29d2b7ed7
OscarGame <458535848@qq.com>
2018-09-06 22:32:37 +0800

    add tcp iocp some file

---
+++ OscarServer/SourceCode/main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+	// start_tcp_listener(5150);
+	//start_server(5150);
+	// start_server(8001);
+	return 0;
+}
